bb5857a9a2cd5cbb0eafbc3c82f9e4ac7aaca8bf
Matthew Pike s3865305 <s3865305@student.rmit.edu.au>
2021-12-16 15:49:52 +1100

    Hover functionality done

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ for (var i = 0; i < 16; i++){
     }
     document.querySelector(".container").appendChild(rowContainer);
 }
+
+var boxes = document.querySelectorAll('.box');
+var boxesArr = Array.from(boxes);
+
+boxesArr.forEach(element => {
+    element.addEventListener('mouseover', function() {
+        element.classList.add('hover');
+    });
+});
