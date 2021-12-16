6c696d2a67b5cf112838936785f359cd3842a8bb
Matthew Pike s3865305 <s3865305@student.rmit.edu.au>
2021-12-16 15:24:57 +1100

    Grid Created

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 for (var i = 0; i < 16; i++){
     var rowContainer = document.createElement('div');
+    rowContainer.classList.add('row');
     var columnContainer;
     for (var j = 0; j < 16; j++){
         columnContainer = document.createElement('div');
